77368ad7cf9ce9f19ab8bb3116955c556bd4c114
GrygrFlzr <git@cybeast.dev>
2025-04-17 21:52:01 +0700

    Proposal team scoring previews

---
+++ src/routes/season-3/proposals/+page.svelte
@@ -0,0 +1,155 @@
+<script lang="ts">
+	import type { PageProps } from './$types';
+    let { data }: PageProps = $props();
+
+    let ayaviProposal = data.teams.map(team => {
+        let grandsPrix = data.grandsPrix.slice(0,4).map((gpName, gpIndex) => {
+            const gpNum = gpIndex + 1;
+            const records = data.scores.filter(
+                record => record.round === gpNum && record.team === team.name
+            )
+            .sort((a, b) => b.score - a.score)
+            .slice(0, 3);
+            return {
+                round: gpNum,
+                name: gpName,
+                records: records,
+                score: records.map(rec => rec.score).reduce((a, b) => a + b, 0),
+            };
+        });
+        return {
+            name: team.name,
+            grandsPrix,
+            totalScore: grandsPrix.map(gp => gp.score).reduce((a, b) => a + b, 0),
+        };
+    }).sort((a, b) => b.totalScore - a.totalScore);
+    let dakkyunProposal = data.teams.map(team => {
+        let grandsPrix = data.grandsPrix.slice(0,4).map((gpName, gpIndex) => {
+            const gpNum = gpIndex + 1;
+            const records = data.scores.filter(
+                record => record.round === gpNum && record.team === team.name
+            )
+            .sort((a, b) => b.score - a.score);
+            return {
+                round: gpNum,
+                name: gpName,
+                records: records,
+                score: records.length === 0 ? 0 : records.map(rec => rec.score).reduce((a, b) => a + b, 0) / records.length,
+            };
+        });
+        return {
+            name: team.name,
+            grandsPrix,
+            totalScore: grandsPrix.map(gp => gp.score).reduce((a, b) => a + b, 0),
+        };
+    }).sort((a, b) => b.totalScore - a.totalScore);
+    let grygrProposal = data.teams.map(team => {
+        let grandsPrix = data.grandsPrix.slice(0,4).map((gpName, gpIndex) => {
+            const gpNum = gpIndex + 1;
+            const records = data.scores.filter(
+                record => record.round === gpNum && record.team === team.name
+            )
+            .sort((a, b) => b.score - a.score);
+            return {
+                round: gpNum,
+                name: gpName,
+                records: records,
+                score: records.map(rec => rec.score).reduce((total, curr, index) => total + (curr * Math.pow(0.1, index)), 0),
+            };
+        });
+        return {
+            name: team.name,
+            grandsPrix,
+            totalScore: grandsPrix.map(gp => gp.score).reduce((a, b) => a + b, 0),
+        };
+    }).sort((a, b) => b.totalScore - a.totalScore);
+</script>
+
+<svelte:head>
+    <meta property="og:title" content="Team Scoring Proposals" />
+    <meta property="og:description" content="Previews of three separate proposals to team scoring" />
+    <title>Season 3 Team Scoring Proposals</title>
+</svelte:head>
+
+<h2>Ayavi's Proposal (adjusted): Count Top 3 individuals per round</h2>
+<p>Per Grand Prix, only the top 3 scores count</p>
+<div class="team-list">
+    {#each ayaviProposal.slice(0, 4) as team (team.name)}
+        <div class="team-entry" style="
+            --color: {data.teams.find(_team => _team.name === team.name)?.color || 'red'};
+        ">
+            <h4>{team.name} ({team.totalScore})</h4>
+            <div>
+                {#each team.grandsPrix as gp (gp.round)}
+                    <h4 class="gp-name">{gp.name} ({gp.score})</h4>
+                    <ol>
+                        {#each gp.records as record (record.name)}
+                            <li>{record.name} ({record.score})</li>
+                        {/each}
+                    </ol>
+                {/each}
+            </div>
+        </div>
+    {/each}
+</div>
+
+<h2>Dakkyun's Proposal: Average per Round</h2>
+<p>Per Grand Prix, all scores are averaged</p>
+<p>As seen below, this yields some funny results for solo teams.</p>
+<div class="team-list">
+    {#each dakkyunProposal.slice(0, 4) as team (team.name)}
+        <div class="team-entry" style="
+        --color: {data.teams.find(_team => _team.name === team.name)?.color || 'red'};
+        ">
+            <h4>{team.name} ({team.totalScore})</h4>
+            <div>
+                {#each team.grandsPrix as gp (gp.round)}
+                    <h4 class="gp-name">{gp.name} ({gp.score})</h4>
+                    <ol>
+                        {#each gp.records as record (record.name)}
+                            <li>{record.name} ({record.score})</li>
+                        {/each}
+                    </ol>
+                {/each}
+            </div>
+        </div>
+    {/each}
+</div>
+
+<h2>Grygr's Proposal: Diminishing Returns</h2>
+<p>Per Grand Prix, 1st score is worth 100%, 2nd is worth 10%, 3rd is worth 1%, ...</p>
+<div class="team-list">
+    {#each grygrProposal.slice(0, 4) as team (team.name)}
+        <div class="team-entry" style="
+            --color: {data.teams.find(_team => _team.name === team.name)?.color || 'red'};
+        ">
+            <h4>{team.name} ({Math.floor(team.totalScore * 100) / 100})</h4>
+            <div>
+                {#each team.grandsPrix as gp (gp.round)}
+                    <h4 class="gp-name">{gp.name} ({Math.floor(gp.score * 100) / 100})</h4>
+                    <ol>
+                        {#each gp.records as record, recordIndex (record.name)}
+                            <li>{record.name} ({record.score} &times; 0.1<sup>{recordIndex}</sup> &equals; {Math.floor(
+                                record.score * Math.pow(0.1, recordIndex) * Math.pow(10, 8)
+                            ) / Math.pow(10, 8)})</li>
+                        {/each}
+                    </ol>
+                {/each}
+            </div>
+        </div>
+    {/each}
+</div>
+
+<style>
+    .team-list {
+        display: grid;
+        grid-template-columns: 1fr 1fr 1fr 1fr;
+    }
+    .team-entry {
+        background: hsl(from var(--color) h s 80%);
+        color: hsl(from var(--color) h s 20%);
+    }
+    ol {
+        padding-left: 1rem;
+    }
+</style>
---
+++ src/routes/season-3/proposals/+page.ts
@@ -0,0 +1,170 @@
+import type { PageLoad } from './$types';
+import pekomama from '$lib/assets/pekomama.jpg?no-inline';
+
+export const load: PageLoad = async ({ parent }) => {
+    const sheetData = (await parent()).sheetData;
+
+    const grandsPrix = [
+        "Australian Grand Prix",
+        "Chinese Grand Prix",
+        "Japanese Grand Prix",
+        "Bahrain Grand Prix",
+        "Saudi Arabian Grand Prix",
+        "Miami Grand Prix",
+        "Emilia Romagna Grand Prix",
+        "Monaco Grand Prix",
+        "Spanish Grand Prix",
+        "Canadian Grand Prix",
+        "Austrian Grand Prix",
+        "British Grand Prix",
+        "Belgian Grand Prix",
+        "Hungarian Grand Prix",
+        "Dutch Grand Prix",
+        "Italian Grand Prix",
+        "Azerbaijan Grand Prix",
+        "Singapore Grand Prix",
+        "United States Grand Prix",
+        "Mexico City Grand Prix",
+        "São Paulo Grand Prix",
+        "Las Vegas Grand Prix",
+        "Qatar Grand Prix",
+        "Abu Dhabi Grand Prix",
+    ];
+
+    type Team = {
+        name: string;
+        color: string;
+        repImage: string;
+    };
+    const teams: Team[] = [
+        {
+            name: "Scuderia KFP",
+            color: '#ff411c',
+            repImage: 'https://hololive.hololivepro.com/wp-content/uploads/2020/07/Takanashi-Kiara_list_thumb.png',
+        },
+        {
+            name: "Amelia's Driver Bureau",
+            color: '#f8db92',
+            repImage: 'https://hololive.hololivepro.com/wp-content/uploads/2020/07/Watson-Amelia_list_thumb.png',
+        },
+        {
+            name: "AMG Reine's [Reidacted]",
+            color: '#0f53ba',
+            repImage: 'https://hololive.hololivepro.com/wp-content/uploads/2020/07/Pavolia-Reine_list_thumb.png',
+        },
+        {
+            name: "GULF Poyoyo Racing",
+            color: 'hsl(353, 83.3%, 57.8%)',
+            repImage: 'https://hololive.hololivepro.com/wp-content/uploads/2020/06/Nakiri-Ayame_list_thumb.png',
+        },
+        {
+            name: "Lamy's Land of Lawnmowers",
+            color: '#6abadf',
+            repImage: 'https://hololive.hololivepro.com/wp-content/uploads/2020/06/Yukihana-Lamy_list_thumb.png',
+        },
+        {
+            name: "IRyS' Stage Racing",
+            color: '#ff0335',
+            repImage: 'https://hololive.hololivepro.com/wp-content/uploads/2020/07/IRyS_list_thumb.png',
+        },
+        {
+            name: "Shiranui Flare's Elfriend F1",
+            color: '#fe3d1c',
+            repImage: 'https://hololive.hololivepro.com/wp-content/uploads/2020/06/Shiranui-Flare_list_thumb.png',
+        },
+        {
+            name: "Nerissa's Birdgarage",
+            color: 'hsl(235.3, 96.4%, 55.9%)',
+            repImage: 'https://hololive.hololivepro.com/wp-content/uploads/2021/07/Nerissa-Ravencroft_list_thumb.png',
+        },
+        {
+            name: "AZKi Pioneer Racing",
+            color: '#fc3488',
+            repImage: 'https://hololive.hololivepro.com/wp-content/uploads/2020/06/AZKi_list_thumb.png',
+        },
+        {
+            name: "Mumei's Sancturacing",
+            color: 'hsl(25.7, 37.5%, 78%)',
+            repImage: 'https://hololive.hololivepro.com/wp-content/uploads/2020/07/Nanashi-Mumei_list_thumb.png',
+        },
+        {
+            name: "Alfa Roneo Racing",
+            color: 'hsl(33, 100%, 68.2%)',
+            repImage: 'https://hololive.hololivepro.com/wp-content/uploads/2020/06/Momosuzu-Nene_list_thumb.png',
+        },
+        {
+            name: "Nun-Nun Speedstar Racing",
+            color: '#266aff',
+            repImage: 'https://hololive.hololivepro.com/wp-content/uploads/2021/05/tokino_sora_thumb.png',
+        },
+        {
+            name: "Shiorin Aramco Racing",
+            color: 'hsl(255.7, 21.9%, 58.8%)',
+            repImage: 'https://hololive.hololivepro.com/wp-content/uploads/2021/07/Shiori-Novella_list_thumb.png',
+        },
+        {
+            name: "Melfriend Formula 1 Team",
+            color: 'hsl(43.6, 85.2%, 65.5%)',
+            repImage: 'https://hololive.hololivepro.com/wp-content/uploads/2020/07/Anya-Melfissa_list_thumb.png',
+        },
+        {
+            name: "HoloMoms Racing Supremacy",
+            color: 'hsl(202.7, 100%, 76.7%)',
+            repImage: pekomama,
+        },
+        {
+            name: "Rindo Chihaya's Garage",
+            color: 'hsl(180, 54.4%, 47.3%)',
+            repImage: 'https://hololive.hololivepro.com/wp-content/uploads/2023/09/Rindo-Chihaya_list_thumb.png',
+        },
+    ];
+
+    const scores = sheetData
+        .split('\n')
+        .slice(3)               // remove headers
+        .flatMap(
+            row => {
+                const fields = row.split('\t');
+                const name = fields[0];
+                return [
+                    {
+                        name: name,
+                        round: 1,
+                        team: fields[1],
+                        score: Number.parseInt(fields[2]) || 0,
+                    },
+                    {
+                        name: name,
+                        round: 2,
+                        team: fields[12 + 1],
+                        score: Number.parseInt(fields[12 + 2]) || 0,
+                    },
+                    {
+                        name: name,
+                        round: 3,
+                        team: fields[24 + 1],
+                        score: Number.parseInt(fields[24 + 2]) || 0,
+                    },
+                    {
+                        name: name,
+                        round: 4,
+                        team: fields[36 + 1],
+                        score: Number.parseInt(fields[36 + 2]) || 0,
+                    },
+                ]
+            }
+        )
+        .filter(score => score.team !== "-");
+
+    const individuals = sheetData
+        .split('\n')
+        .slice(3)
+        .map(row => row.split('\t')[0]);
+
+    return {
+        scores,
+        teams,
+        individuals,
+        grandsPrix,
+    };
+};

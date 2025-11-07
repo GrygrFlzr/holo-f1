ef1564c58895d5eef1f0af578d3946dbbec2a0b4
GrygrFlzr <git@cybeast.dev>
2025-11-07 23:46:25 +0700

    Switch to experimental rolldown-vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,16 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
-import terser from '@rollup/plugin-terser';
 
 export default defineConfig({
 	plugins: [sveltekit()],
 	server: {
 		allowedHosts: ['localhost', '.cybeast.dev']
 	},
-	build: {
-		rollupOptions: {
-			plugins: [
-				terser({
-					ecma: 2020,
-					compress: {
-						ecma: 2020,
-						drop_console: true
-					},
-					format: {
-						ecma: 2020,
-						comments: false
-					}
-				})
-			]
+	worker: {
+		rolldownOptions: {
+			output: {
+				minify: true
+			}
 		}
 	}
 });

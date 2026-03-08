f6dea3c8c513f2909db7ffcc2772dde10468fc95
GrygrFlzr <git@cybeast.dev>
2026-03-08 21:59:51 +0700

    fix: explicit woff2-only font load

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
 	import { page } from '$app/state';
-	import '@fontsource/titillium-web/200.css';
-	import '@fontsource/titillium-web/300.css';
-	import '@fontsource/titillium-web/400.css';
-	import '@fontsource/titillium-web/600.css';
-	import '@fontsource/titillium-web/700.css';
+	import './fonts.css';
 	import './colors.css';
 
 	let { children } = $props();

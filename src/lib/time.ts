b315d0bb0758a78ae9863865941f17b2a5cdd447
GrygrFlzr <git@cybeast.dev>
2026-03-12 15:30:17 +0700

    fix: make lib/time easier to minify

--- src/lib/time.ts
+++ src/lib/time.ts
@@ -1,17 +1,17 @@
-type TimeKey = 'year' | 'month' | 'date' | 'hour' | 'minute' | 'second';
-const patterns: Record<TimeKey, string> = {
-	year: String.raw`(?<year>\d{4})`,
-	month: String.raw`(?<month>(?:0[1-9]|1[012]))`,
-	date: String.raw`(?<date>(?:0[1-9]|[12]\d|3[01]))`,
-	hour: String.raw`(?<hour>(?:[01]\d|2[0123]))`,
-	minute: String.raw`(?<minute>[0-5]\d)`,
-	second: String.raw`(?<second>[0-5]\d(?:\.\d{3})?)`
-};
+type TimeKey = 'yy' | 'mm' | 'dd' | 'hh' | 'ii' | 'ss';
+const { raw } = String;
+const yearPattern = raw`(?<yy>\d{4})`;
+const monthPattern = raw`(?<mm>(?:0[1-9]|1[012]))`;
+const datePattern = raw`(?<dd>(?:0[1-9]|[12]\d|3[01]))`;
+const hourPattern = raw`(?<hh>(?:[01]\d|2[0123]))`;
+const minutePattern = raw`(?<ii>[0-5]\d)`;
+const secondPattern = raw`(?<ss>[0-5]\d(?:\.\d{3})?)`;
+
 const isoTimePattern = new RegExp(
-	`^${patterns.year}-${patterns.month}-${patterns.date}T${patterns.hour}:${patterns.minute}:${patterns.second}Z$`
+	`^${yearPattern}-${monthPattern}-${datePattern}T${hourPattern}:${minutePattern}:${secondPattern}Z$`
 );
 const sqliteTimePattern = new RegExp(
-	`^${patterns.year}-${patterns.month}-${patterns.date} ${patterns.hour}:${patterns.minute}:${patterns.second}$`
+	`^${yearPattern}-${monthPattern}-${datePattern} ${hourPattern}:${minutePattern}:${secondPattern}$`
 );
 
 /*@__NO_SIDE_EFFECTS__*/
@@ -23,8 +23,8 @@ export function parseDateTime(datetimeStr: string): Date | null {
 	} else {
 		const match = sqliteTimePattern.exec(datetimeStr);
 		if (match === null) return null;
-		const { year, month, date, hour, minute, second } = match.groups as Record<TimeKey, string>;
-		maybeDate = new Date(`${year}-${month}-${date}T${hour}:${minute}:${second}Z`);
+		const { yy, mm, dd, hh, ii, ss } = match.groups as Record<TimeKey, string>;
+		maybeDate = new Date(`${yy}-${mm}-${dd}T${hh}:${ii}:${ss}Z`);
 	}
 	// block invalid dates like Feb 30
 	if (isNaN(maybeDate.getTime())) return null;

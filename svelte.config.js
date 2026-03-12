3b258bd38dd0e02273340438882c33d04b757e7c
GrygrFlzr <git@cybeast.dev>
2026-03-12 15:33:35 +0700

    chore: flip modernAst on

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,9 @@ import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	compilerOptions: {
+		modernAst: true
+	},
 	kit: {
 		adapter: adapter({
 			platformProxy: {

09b27e59ded22d070903fb378121677845cbb096
GrygrFlzr <git@cybeast.dev>
2026-03-27 12:58:59 +0700

    feat: default to runes mode

--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,27 @@
 import { execSync } from 'node:child_process';
+import { relative, sep } from 'node:path';
 import adapter from '@sveltejs/adapter-cloudflare';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	compilerOptions: {
-		modernAst: true
+		modernAst: true,
+		runes: ({ filename }) => {
+			const relativePath = relative(import.meta.dirname, filename);
+			const pathSegments = relativePath.toLowerCase().split(sep);
+			const isExternalLibrary = pathSegments.includes('node_modules');
+			const isKitErrorPagePath = pathSegments
+				.join('/')
+				.endsWith('@sveltejs/kit/src/runtime/components/svelte-5/error.svelte');
+
+			if (isKitErrorPagePath) {
+				return true;
+			} else if (isExternalLibrary) {
+				return undefined;
+			}
+			// user code
+			return true;
+		}
 	},
 	kit: {
 		adapter: adapter({

23e0ce803571fa4da7e8e3a5fe9c8257e07e5a86
GrygrFlzr <git@cybeast.dev>
2026-05-21 09:39:33 +0700

    data: swap to canadian GP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -65,7 +65,7 @@
 	</nav>
 	<ul>
 		<li>
-			<a href={resolve('/submit')}>Submit a prediction for the Miami GP</a>
+			<a href={resolve('/submit')}>Submit a prediction for the Canadian GP</a>
 		</li>
 		<li>
 			Check the Season 3 archives:

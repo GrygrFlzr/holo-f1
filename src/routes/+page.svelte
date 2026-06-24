803bfae05fc7268345e94ec7085c09e2d20fb624
GrygrFlzr <git@cybeast.dev>
2026-06-24 16:56:37 +0700

    bump homepage GP

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -65,7 +65,7 @@
 	</nav>
 	<ul>
 		<li>
-			<a href={resolve('/submit')}>Submit a prediction for the Barcelona GP</a>
+			<a href={resolve('/submit')}>Submit a prediction for the Austrian GP</a>
 		</li>
 		<li>
 			Check the Season 3 archives:
